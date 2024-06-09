1f9611a559190b7b87c3fb25d9842028e40925be
Falme Streamless <falme_streamless@hotmail.com>
2024-06-09 09:28:43 -0300

    Added verifier to go back to idle animation after finishes an attack or hurt

--- Scripts/Character/character.gd
+++ Scripts/Character/character.gd
@@ -19,3 +19,12 @@ func play_hurt():
 func play_death():
 	$CharacterAnimation.play("character_animation/character_dying")
 	pass
+
+
+func _on_character_animation_animation_finished(anim_name):
+	if anim_name != "character_animation/character_dying":
+		play_idle()
+
+
+func _on_character_animation_animation_started(anim_name):
+	pass
